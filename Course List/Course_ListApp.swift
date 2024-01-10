76262c02052259e79949599c0192fc4a8c2a2b8b
Andy Dobbs <andrew.dobbs@gmail.com>
2024-01-10 14:46:15 -0500

    Initial Commit

---
+++ Course List/Course_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Course_ListApp.swift
+//  Course List
+//
+//  Created by Andy Dobbs on 1/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Course_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
